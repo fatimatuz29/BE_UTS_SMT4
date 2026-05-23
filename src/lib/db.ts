775d867c6d5c33de6a31e8d95276f3089a1df4c3
FatimatuzZahro <f56446473@gmail.com>
2026-05-23 21:55:15 +0700

    fix db conflict

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
+
 import { PrismaClient } from "@prisma/client";
 
-=======
-import { PrismaClient } from "@prisma/client";
-
->>>>>>> 2fac8cfae754bc637848f97e3af7df95ca5fd8ff
 export const prisma = new PrismaClient();
