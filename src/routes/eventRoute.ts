e6b3a0e0d94e335b01b2629f6ad13f1f86c4f145
FatimatuzZahro <f56446473@gmail.com>
2026-05-23 21:38:39 +0700

    fix marge conflict

--- src/routes/eventRoute.ts
+++ src/routes/eventRoute.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
     getEvents,
     createEvent,
@@ -10,29 +9,10 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", getEvents); //menampilkan data event
-router.post("/", createEvent); //menyimpan data event
-router.get("/:id", getEventById); //menampilkan event by  id
-router.put("/:id", updateEvent); //mengupdate data event by id
-router.delete("/:id", deleteEvent); //menghapus data event by id
+router.get("/", getEvents);
+router.post("/", createEvent);
+router.get("/:id", getEventById);
+router.put("/:id", updateEvent);
+router.delete("/:id", deleteEvent);
 
-=======
-import {
-    getEvents,
-    createEvent,
-    getEventById,
-    updateEvent,
-    deleteEvent,
-} from "./../controllers/eventController";
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", getEvents); //menampilkan data event
-router.post("/", createEvent); //menyimpan data event
-router.get("/:id", getEventById); //menampilkan event by  id
-router.put("/:id", updateEvent); //mengupdate data event by id
-router.delete("/:id", deleteEvent); //menghapus data event by id
-
->>>>>>> 2fac8cfae754bc637848f97e3af7df95ca5fd8ff
 export default router;
