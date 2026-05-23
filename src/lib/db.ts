93405b28e3e25566bbdbebe1d55b92ec7f65c0c6
FatimatuzZahro <f56446473@gmail.com>
2026-05-23 22:49:49 +0700

    coba lagi

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient({
     datasources: {
         db: {
-            url: process.env.DATABASE_URL,
+            url: "postgresql://postgres.iqeebafnkugqdyrsiscp:Invofest2026@aws-1-ap-south-1.pooler.supabase.com:5432/postgres",
         },
     },
 });
