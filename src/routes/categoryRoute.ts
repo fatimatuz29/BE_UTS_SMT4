445e503801209b9e3a3cdae845de65f9ce8c485d
FatimatuzZahro <f56446473@gmail.com>
2026-05-23 21:43:06 +0700

    fix merge conflict all routes

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
     getCategories,
     createCategory,
@@ -10,29 +9,10 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", getCategories); //menampilkan data categori
-router.post("/", createCategory); //menyimpan data categori
-router.get("/:id", getCategoryById); //menampilkan categori by  id
-router.put("/:id", updateCategory); //mengupdate data categori by id
-router.delete("/:id", deleteCategory); //menghapus data categori by id
+router.get("/", getCategories);
+router.post("/", createCategory);
+router.get("/:id", getCategoryById);
+router.put("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
 
-=======
-import {
-    getCategories,
-    createCategory,
-    getCategoryById,
-    updateCategory,
-    deleteCategory,
-} from "./../controllers/CategoryControllers";
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", getCategories); //menampilkan data categori
-router.post("/", createCategory); //menyimpan data categori
-router.get("/:id", getCategoryById); //menampilkan categori by  id
-router.put("/:id", updateCategory); //mengupdate data categori by id
-router.delete("/:id", deleteCategory); //menghapus data categori by id
-
->>>>>>> 2fac8cfae754bc637848f97e3af7df95ca5fd8ff
 export default router;
--- src/routes/pembicaraRoutes.ts
+++ src/routes/pembicaraRoutes.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import {
     getPembicara,
     getPembicaraById,
@@ -16,23 +16,5 @@ router.get("/:id", getPembicaraById); //menampilkan pembicara by id
 router.put("/:id", updatePembicara); //mengupdate data pembicara by id
 router.delete("/:id", deletePembicara); //menghapus data pembicara by id
 
-=======
-import {
-    getPembicara,
-    getPembicaraById,
-    createPembicara,
-    updatePembicara,
-    deletePembicara,
-} from "../controllers/pembicaraController";
-import express from "express";
 
-const router = express.Router ();
-
-router.get("/", getPembicara); //menampilkan data pembicara
-router.post("/", createPembicara); //menyimpan data pembicara
-router.get("/:id", getPembicaraById); //menampilkan pembicara by id
-router.put("/:id", updatePembicara); //mengupdate data pembicara by id
-router.delete("/:id", deletePembicara); //menghapus data pembicara by id
-
->>>>>>> 2fac8cfae754bc637848f97e3af7df95ca5fd8ff
 export default router;
